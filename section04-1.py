d86d91764a9a078077bcb34a22439fe8d740637c
khy0425 <from_ap@naver.com>
2020-11-28 17:42:57 +0900

    첫 커밋

---
+++ section04-1.py
@@ -0,0 +1,14 @@
+v_str1 = "Nice man"
+v_bool = True
+v_str2 = "Goodboy"
+v_float = 10.3
+v_int = 7
+v_dict = {
+    "name" : "kim",
+    "age" : 25,
+}
+v_list = [3, 5, 7]
+v_tuple = 3, 5, 7
+v_set = {7, 8, 9}
+
+print(type(v_bool), type(v_str1), type(v_str2), type(v_dict), type(v_tuple), type(v_set))
